00f57d9363a883393ce1c46c1cc34874f1a42763
Egregius <guy@egregius.be>
2014-12-30 14:08:36 +0100

    20141230

--- parameters.php
+++ parameters.php
@@ -1,23 +1,21 @@
 <?php
-
 // Make database connection ('server', 'database', 'password', 'user') 
 $db = new mysqli('server', 'database', 'password', 'user');
 if($db->connect_errno > 0){ die('Unable to connect to database [' . $db->connect_error . ']');}
 
-// Url to homewizard "http://address:port/password/"
-$jsonurl = "http://address:port/password/";
-
-//simple authentication when not at home
-$valid_passwords = array ("home" => "wizard");
-
-//IP Address array that bypass authentication
-$acceptedip=array("1.2.3.4","5.6.7.8");
-//USername and password for external access
-$secretusername = '1234';
-$secretpassword = '1234';
+	$sql="select variable, value from settings order by variable asc";
+	if(!$result = $db->query($sql)){ die('There was an error running the query [' . $db->error . ']');}
+	$acceptedips = array();
+	while($row = $result->fetch_assoc()){
+		$$row['variable'] = $row['value'];
+		if (strpos($row['variable'], 'acceptedip') === 0) { 
+			array_push($acceptedips, $row['value']);
+		}
+	}
+	$result->free();
 
 $authenticated = false;
-//if(in_array($_SERVER['REMOTE_ADDR'], $acceptedip)) $authenticated = true;
+if(in_array($_SERVER['REMOTE_ADDR'], $acceptedips)) $authenticated = true; 
 session_start();
 if(isset($_SESSION['authenticated'])) {
 	if ($_SESSION['authenticated'] == true) {
--- settings.php
+++ settings.php
@@ -27,4 +27,32 @@ if($authenticated==true) {
 }
 print '</div>';
 include "footer.php";
+?>d'];
+			if ($username == $secretusername && $password == $secretpassword) {
+			$_SESSION['authenticated'] = true;
+			$authenticated = true;
+		} else {
+			$error = 'Incorrect username or password';
+		}
+	}
+   echo '<p class="error">'.$error.'</p>';
+	}
+if($authenticated==true) {
+	echo '<center><table width="400px" style="text-align:center"><tbody>';
+	$sql="select variable, value from settings order by variable asc";
+	if(!$result = $db->query($sql)){ die('There was an error running the query [' . $db->error . ']');}
+	while($row = $result->fetch_assoc()){
+		echo '<form method="post"><tr><td>'.$row['variable'].'</td><td><input type="hidden" name="variable" id="variable" value="'.$row['variable'].'"><input type="text" name="value" id="value" value="'.$row['value'].'"></td><td><input type="submit" name="upd" value="upd" class="btn btn-default"></td></tr></form>';
+	}
+	$result->free();
+	echo '<form method="post"><tr><td><input type="text" name="variable" id="variable" value=""></td><td><input type="text" name="value" id="value" value=""></td><td><input type="submit" name="add" value="add" class="btn btn-default"></td></tr></form></tbody></table></center>';
+	
+	print '<form method="post"><input type="submit" name="logout" value="logout" class="abutton"/></form>';
+} else {
+	print '<br/><br/>Log in:<br/><br/>';
+	print '<br/><br/><form method="post"><input type="text" name="username" /><input type="password" name="password" /><input type="submit" value="login" class="abutton"/></form>';
+}
+$db->close();
+print '</div></div>';
+include "footer.php";
 ?>
