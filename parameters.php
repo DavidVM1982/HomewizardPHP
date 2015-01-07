1784aa74fcc63cbcc746fe5d4f50902ff1a5a451
Egregius <guy@egregius.be>
2015-01-07 14:26:55 +0100

    Fix
    Correct order server/user/pswd/db

--- parameters.php
+++ parameters.php
@@ -1,5 +1,5 @@
 <?php
-// Make database connection ('server', 'database', 'password', 'user') 
-$db = new mysqli('server', 'database', 'password', 'user');
+// Make database connection ('server', 'user', 'password', 'database') 
+$db = new mysqli('server', 'user', 'password', 'database');
 if($db->connect_errno > 0){ die('Unable to connect to database [' . $db->connect_error . ']');}
 ?>
