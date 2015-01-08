c40a35695f111d745e5bfd7381bf5a41ffa46864
Guy Verschuere <guy@egregius.be>
2015-01-08 17:05:06 +0100

    Update update_20150108.sql

--- database/update_20150108.sql
+++ database/update_20150108.sql
@@ -1,1 +1,2 @@
 UPDATE `homewizard`.`statusses` SET `omschrijving` = 'Gesloten' WHERE `statusses`.`status` = 'contactno';
+UPDATE `homewizard`.`statusses` SET `omschrijving` = 'ROOK!!!' WHERE `statusses`.`status` = 'smokeyes';
