d791ae822987e498aaa6fa32cfdfbb1d068104db
Guy Verschuere <guy@egregius.be>
2015-01-08 17:31:36 +0100

    Update create-database_20150108.sql

--- database/create-database_20150108.sql
+++ database/create-database_20150108.sql
@@ -108,3 +108,18 @@ INSERT IGNORE INTO `statusses` (`status`, `omschrijving`) VALUES
 ('motionyes', 'Beweging'),
 ('smokeno', 'Getest'),
 ('smokeyes', 'ROOK!!!');
+
+CREATE TABLE IF NOT EXISTS `versie` (
+`id` int(11) NOT NULL,
+  `versie` int(11) NOT NULL,
+  `datumupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `versie`
+ ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `versie`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+
+INSERT INTO `versie` (`id`, `versie`) VALUES
+(1, 20150108);
