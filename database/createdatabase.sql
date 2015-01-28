eda3b82c90dbbde8c0f30e6a9a0ce66768be2c0e
Guy Verschuere <guy@egregius.be>
2015-01-28 16:40:19 +0100

    Update createdatabase.sql

--- database/createdatabase.sql
+++ database/createdatabase.sql
@@ -1,6 +1,14 @@
 CREATE DATABASE IF NOT EXISTS `homewizard` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `homewizard`;
 
+CREATE TABLE IF NOT EXISTS `energylink` (
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `netto` float NOT NULL,
+  `S1` float NOT NULL,
+  `S2` float NOT NULL,
+  `gas` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE IF NOT EXISTS `history` (
   `id_sensor` smallint(6) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -39,6 +47,12 @@ CREATE TABLE IF NOT EXISTS `switches` (
   `volgorde` smallint(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS `switchhistory` (
+  `id_switch` smallint(6) NOT NULL,
+  `timestamp` int(11) NOT NULL,
+  `type` varchar(50) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS `temperature` (
   `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `te` float NOT NULL,
@@ -57,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `versie` (
 `id` int(11) NOT NULL,
   `versie` int(11) NOT NULL,
   `datumupdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `wind` (
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -72,6 +86,10 @@ CREATE TABLE IF NOT EXISTS `wind_day` (
   `id_sensor` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+
+ALTER TABLE `energylink`
+ ADD PRIMARY KEY (`timestamp`);
+
 ALTER TABLE `history`
  ADD PRIMARY KEY (`id_sensor`,`time`);
 
@@ -79,7 +97,7 @@ ALTER TABLE `rain`
  ADD PRIMARY KEY (`date`,`id_sensor`);
 
 ALTER TABLE `sensors`
- ADD PRIMARY KEY (`id_sensor`);
+ ADD PRIMARY KEY (`id_sensor`,`type`);
 
 ALTER TABLE `settings`
  ADD PRIMARY KEY (`variable`);
@@ -88,7 +106,7 @@ ALTER TABLE `statusses`
  ADD PRIMARY KEY (`status`);
 
 ALTER TABLE `switches`
- ADD PRIMARY KEY (`id_switch`);
+ ADD PRIMARY KEY (`id_switch`,`type`);
 
 ALTER TABLE `temperature`
  ADD PRIMARY KEY (`timestamp`,`id_sensor`);
@@ -105,8 +123,9 @@ ALTER TABLE `wind`
 ALTER TABLE `wind_day`
  ADD PRIMARY KEY (`date`,`id_sensor`);
 
+
 ALTER TABLE `versie`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
 
 INSERT IGNORE INTO `statusses` (`status`, `omschrijving`) VALUES
 ('contactno', 'Gesloten'),
@@ -121,20 +140,23 @@ INSERT IGNORE INTO `settings` (`variable`, `value`) VALUES
 ('acceptedip', '127.0.0.1'),
 ('acceptedip2', '1.2.3.4'),
 ('debug', 'no'),
-('detailscenes', 'no'),
-('developerjson', '{"status": "ok", "version": "2.81", "request": {"route": "/get-sensors" }, "response": {"switches" : [{"id":0,"name":"Kerstboom","type":"switch","status":"off","favorite":"no"},{"id":1,"name":"Licht Garage","type":"switch","status":"on","favorite":"no"},{"id":2,"name":"Bureel Tobi","type":"switch","status":"off","favorite":"no"},{"id":3,"name":"Lamp Bureel","type":"switch","status":"off","favorite":"no"},{"id":4,"name":"TV","type":"switch","status":"off","favorite":"yes"},{"id":5,"name":"Radio","type":"switch","status":"on","favorite":"yes"},{"id":6,"name":"Badkamer","type":"radiator","tte":15.0,"favorite":"no"},{"id":7,"name":"Slaapkamer","type":"radiator","tte":15.0,"favorite":"no"},{"id":8,"name":"Slaapkamer Tobi","type":"radiator","tte":15.0,"favorite":"no"},{"id":9,"name":"Diskstation","type":"virtual","status":"on","favorite":"no"},{"id":10,"name":"Living","type":"dimmer","status":"off","dimlevel":0,"favorite":"no"},{"id":11,"name":"Salon","type":"dimmer","status":"off","dimlevel":0,"favorite":"no"}],"uvmeters":[],"windmeters":[{"id":2,"name":"Windmeter","code":"10321553","model":1,"lowBattery":"no","version":2.19,"unit":0,"ws":3.0,"dir":"ESE 112","gu":4.3,"wc":8.4,"te":8.4,"ws+":7.0,"ws+t":"00:40","ws-":0.1,"ws-t":"10:12","favorite":"no"}],"rainmeters":[{"id":3,"name":"Regenmeter","code":"4091779","model":1,"lowBattery":"no","version":2.19,"mm":15.4,"3h":0.0,"favorite":"no"}],"thermometers":[{"id":1,"name":"Buiten","code":"13666960","model":1,"lowBattery":"no","version":2.19,"te":9.5,"hu":81,"te+":10.5,"te+t":"01:36","te-":9.0,"te-t":"09:56","hu+":90,"hu+t":"07:04","hu-":81,"hu-t":"16:07","outside":"yes","favorite":"no"}],"weatherdisplays":[{"id":0,"name":"Weerstation","code":"11657828","model":1,"version":2.20,"favorite":"no"}], "energymeters": [], "energylinks": [], "heatlinks": [], "hues": [], "scenes": [{"id": 0, "name": "Alles", "favorite": "yes"}], "kakusensors": [{"id":0,"name":"Zolder","status":null,"type":"smoke","favorite":"no","timestamp":"00:00","cameraid":null},{"id":1,"name":"Poort","status":"no","type":"contact","favorite":"no","timestamp":"11:25","cameraid":null},{"id":2,"name":"Garage","status":"no","type":"motion","favorite":"no","timestamp":"15:27","cameraid":null},{"id":3,"name":"Hal boven","status":null,"type":"smoke","favorite":"no","timestamp":"00:00","cameraid":null},{"id":4,"name":"Deurbel","status":null,"type":"doorbell","favorite":"no","timestamp":"00:00","cameraid":null}], "cameras": []}}'),
+('defaultthermometer', '1'),
+('detailscenes', 'optional'),
+('developerjson', '{"status": "ok", "version": "2.84", "request": {"route": "/get-sensors" }, "response": {"switches" : [{"id":0,"name":"Pluto","type":"switch","status":"off","favorite":"yes"},{"id":1,"name":"Licht Garage","type":"switch","status":"off","favorite":"no"},{"id":2,"name":"Bureel Tobi","type":"switch","status":"off","favorite":"no"},{"id":3,"name":"Lamp Bureel","type":"switch","status":"on","favorite":"no"},{"id":4,"name":"TV","type":"switch","status":"off","favorite":"yes"},{"id":5,"name":"Radio","type":"switch","status":"on","favorite":"yes"},{"id":6,"name":"Badkamer","type":"radiator","tte":10.0,"favorite":"yes"},{"id":7,"name":"Slaapkamer","type":"radiator","tte":8.0,"favorite":"no"},{"id":8,"name":"Slaapkamer Tobi","type":"radiator","tte":8.0,"favorite":"no"},{"id":9,"name":"Diskstation","type":"virtual","status":"off","favorite":"no"},{"id":10,"name":"Eettafel","type":"dimmer","status":"off","dimlevel":0,"favorite":"no"},{"id":11,"name":"Zithoek","type":"dimmer","status":"off","dimlevel":0,"favorite":"no"},{"id":12,"name":"Brander","type":"switch","status":"off","favorite":"no"},{"id":13,"name":"Zonneluifel","type":"somfy","favorite":"no"},{"id":14,"name":"Eetplaats","type":"radiator","tte":15.0,"favorite":"no"},{"id":15,"name":"Zitplaats","type":"radiator","tte":8.0,"favorite":"no"}],"uvmeters":[],"windmeters":[{"id":2,"name":"Windmeter","code":"10321553","model":1,"lowBattery":"yes","version":2.19,"unit":0,"ws":0.1,"dir":"NNW 337","gu":0.0,"wc":5.5,"te":5.5,"ws+":3.2,"ws+t":"14:35","ws-":0.1,"ws-t":"18:26","favorite":"no"}],"rainmeters":[{"id":3,"name":"Regenmeter","code":"4091779","model":1,"lowBattery":"no","version":2.19,"mm":0.0,"3h":0.0,"favorite":"no"}],"thermometers":[{"id":1,"name":"Buiten","code":"13666960","model":1,"lowBattery":"no","version":2.19,"te":6.4,"hu":83,"te+":8.0,"te+t":"14:48","te-":2.2,"te-t":"06:44","hu+":89,"hu+t":"05:39","hu-":69,"hu-t":"14:32","outside":"yes","favorite":"no"},{"id":4,"name":"Badkamer","channel":1,"model":0,"outside":"no","favorite":"no"}],"weatherdisplays":[{"id":0,"name":"Weerstation","code":"11657828","model":1,"version":2.20,"favorite":"no"}], "energymeters": [], "energylinks": [], "heatlinks": [], "hues": [], "scenes": [{"id": 0, "name": "Alles", "favorite": "yes"}], "kakusensors": [{"id":0,"name":"Zolder","status":null,"type":"smoke","favorite":"no","timestamp":"00:00","cameraid":null},{"id":1,"name":"Poort","status":"no","type":"contact","favorite":"no","timestamp":"18:00","cameraid":null},{"id":2,"name":"Garage","status":"no","type":"motion","favorite":"no","timestamp":"18:13","cameraid":null},{"id":3,"name":"Hal boven","status":null,"type":"smoke","favorite":"no","timestamp":"00:00","cameraid":null},{"id":4,"name":"Deurbel","status":null,"type":"doorbell","favorite":"no","timestamp":"00:00","cameraid":null}], "cameras": []}}\r\n\r\n\r\n'),
 ('developermode', 'no'),
-('jsonurl', 'http://adres:port/wachtwoord/'),
+('jsonurl', 'http://adres:port/password/'),
+('positie_energylink', '7'),
 ('positie_radiatoren', '3'),
 ('positie_regen', '8'),
 ('positie_scenes', '2'),
 ('positie_schakelaars', '1'),
 ('positie_sensoren', '4'),
-('positie_somfy', '1'),
+('positie_somfy', '3'),
 ('positie_temperatuur', '6'),
 ('positie_wind', '9'),
-('refreshinterval', '60'),
+('refreshinterval', '30'),
 ('secretpassword', '5678'),
 ('secretusername', '1234');
 
-INSERT INTO `versie` (`versie`) VALUES (20150116)
+INSERT IGNORE INTO `versie` (`versie`, `datumupdate`) VALUES
+(20150128, '2015-01-28 15:13:56');
