75f8d913bbd390d045e3dbdfada0a52993692bc5
Guy Verschuere <guy@egregius.be>
2015-02-08 17:33:03 +0100

    Update createdatabase.sql

--- database/createdatabase.sql
+++ database/createdatabase.sql
@@ -6,7 +6,8 @@ CREATE TABLE IF NOT EXISTS `energylink` (
   `netto` float NOT NULL,
   `S1` float NOT NULL,
   `S2` float NOT NULL,
-  `gas` float NOT NULL
+  `gas` float NOT NULL,
+  `verbruik` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `history` (
